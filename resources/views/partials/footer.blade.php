a4c32b9dd2f1aabfd2edc9be3293cfa3c5f2ea19
rota99C <rotaboolean@gmail.com>
2022-01-06 22:45:03 +0100

    initial commit - creato layout, header e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,72 @@
+<footer class="w-100 position-relative">
+    <img class="footer-img position-absolute" src="{{asset('img/footer-gang.png')}}" alt="">
+    <div class="footer-container h-100 m-auto">
+        <div class="newsletter">
+            <h2 class="text-white"> Want us to email you occasionally with <br> Laracasts news? </h2>
+            <div class="subscribe">
+                <input class="w-25" type="email" placeholder="Enter your email address">
+                <button class="rounded-pill text-white" type="submit"><span>SUBSCRIBE</span> </button>
+            </div>
+        </div>
+        <div class="footer-middle d-flex">
+            <div class="col-6">
+                <img src="{{asset('img/Laracasts-footer.png')}}" alt="">
+                <p class="text-white w-75 mt-3 mb-4">
+                Nine out of ten doctors recommend Laracasts over competing brands. Come inside, see for yourself, and massively level up your development skills in the process.
+                </p>
+                <div class="social">
+                    <img src="{{asset('img/youtube.png')}}" alt="">
+                    <img src="{{asset('img/twitter.png')}}" alt="">
+                    <img src="{{asset('img/social-gatto.png')}}" alt="">
+                </div>
+            </div>
+            <div class="col-6 d-flex">
+                <div class="col-4">
+                    <ul>
+                        <h5>LEARN</h5>
+                        <li>Sign Up</li>
+                        <li>Sign In</li>
+                        <li>Series</li>
+                        <li>Larabits</li>
+                        <li>Topics</li>
+                        <li>What's New</li>
+                        <li>Search</li>
+                        <li>Commercial</li>
+
+                    </ul>
+                </div>
+                <div class="col-4">
+                <ul>
+                        <h5>DISCUSS</h5>
+                        <li>Forum</li>
+                        <li>Podcast</li>
+                        <li>Blog</li>
+                        <li>Support</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                <ul>
+                        <h5>EXTRAS</h5>
+                        <li>Gift Certificates</li>
+                        <li>Teams</li>
+                        <li>FAQ</li>
+                        <li>Assets</li>
+                        <li>Get a Job</li>
+                        <li>Privacy</li>
+                        <li>Terms</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <hr>
+        <div class="footer-bottom text-center">
+            <p>&copy; Laracasts 2022. All rights reserved. Yes, all of them. That means you, Todd.</p>
+            <p>Designed with <i class="fas fa-heart"></i> by <strong>Tuds</strong>. Proudly hosted with <strong>Laravel Forge</strong> and <strong>DigitalOcean</strong>.</p>
+        </div>
+
+
+    </div>
+
+
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header class="w-100 d-flex justify-content-between align-items-center">
+    <!-- logo -->
+    <div>
+       <img src="{{asset('img/Laracasts.png')}}" alt="">
+   </div>
+   <!-- navbar -->
+   <nav class="navbar-centrale">
+       <ul>
+           <li><a href="">TOPICS</a></li>
+           <li><a href="">SERIES</a></li>
+           <li><a href="">LARABITS <sup class="rounded-pill">New!</sup></a></li>
+           <li><a href="">DISCUSSIONS</a></li>
+           <li><a href="">PODCAST</a></li>
+       </ul>
+   </nav>
+   <div class="navbar-laterale d-flex align-items-center">
+       <div class="position-relative"><img class="position-absolute top-50 start-50 translate-middle" src="{{asset('img/search-icon.png')}}" alt=""></div>
+       <a href="">SIGN IN</a>
+       <button class="rounded-pill">GET STARTED</button>
+   </div>
+</header>
