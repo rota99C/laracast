d180920da55958664ee64646d5da1652821e0a11
rota99C <rotaboolean@gmail.com>
2022-01-07 23:37:53 +0100

    section recently updated completa e stilizzata

--- resources/views/recently_updated.blade.php
+++ resources/views/recently_updated.blade.php
@@ -5,29 +5,23 @@
     </div>
 
     @foreach ($series_updated['cards'] as $card)
-    <div class="singolo w-75 m-auto d-flex">
+    <div class="singolo w-75 d-flex">
         <div class="contenuto col-8">
             <button class="tag rounded-pill mb-3" value="{{$card['tag']}}">{{$card['tag']}}</button>
             <h3 class="mb-4">{{$card['title']}}</h3>
             <p class="mb-5">{{$card['description']}}</p>
-            <div class="info">
-                <div class="container_level">
-                    <div>
-                        <img src="../img/cards/difficulty_meter.png" alt=""></img>
-                    </div>
-                    <div>
-                        <p>{{$card['level']}}</p>
-                        <p>difficulty</p>
-                    </div>
+            <div class="info d-flex align-items-center">
+                <div class="container_level d-flex align-items-center">
+                    <img src="../img/cards/difficulty_meter.png" alt=""></img>
+                    <span>{{$card['level']}} <br>Difficulty </span>
                 </div>
 
 
-                <i class="fas fa-book"></i>{{$card['lessons']}}
-                <p>Diffiulty</p>
-                <i class="fas fa-clock"></i>{{$card['time']}}
+                <i class="fas fa-book"></i><span>{{$card['lessons']}}</span>
+                <i class="fas fa-clock"></i><span>{{$card['time']}}</span>
             </div>
 
-            <button class=" start rounded-pill"><i class="far fa-play-circle"></i>Start Series</button>
+            <button class=" start rounded-pill mt-4"><i class="far fa-play-circle"></i>Start Series</button>
         </div>
         <div class="immagine col-4">
             <img src="{{$card['image']}}"></img>
